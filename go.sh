ec37dc57a148395616fde907467be7ee8e70cfa6
Brain <brianleke@gmail.com>
2016-01-24 15:05:59 +1100

    [Brain] Adding argument check to bash script

--- go.sh
+++ go.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo $1
+usage(){
+	echo "Illegal number of parameters. Must parse one parameter as follows:"
+	echo -e "Usage: $0 [Argument]"
+}
+
+[ "$#" != "1" ] && ( usage && exit 1 )
 
 python main.py "$1"
+
