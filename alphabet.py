28d7e875a7eb6f14f9bb2058f689759586cb86ee
Brain <brianleke@gmail.com>
2016-01-27 09:16:24 +1100

    [Brain] Adding more cleanup to code removing redundant new lines

--- alphabet.py
+++ alphabet.py
@@ -17,16 +17,16 @@ class Alphabet:
 
 	def _pyramid(self, end_position, start_position):
 		number_of_spaces = end_position - start_position
-		result = self._space(number_of_spaces) + 'A\n'
+		result = self._space(number_of_spaces) + 'A'
 		result += self._append_middle_pyramid(start_position, end_position) 
 		result += self._space(number_of_spaces) + 'A'
 		return result
 
 	def _reverse_pyramid(self, half_pyramid):
 		second_half = ''
-		first_part = half_pyramid.strip('\n').rsplit('\n', 1)
+		first_part = half_pyramid.rsplit('\n', 1)
 		if len(first_part) > 1:
-			second_half += first_part[0][::-1] + '\n'
+			second_half += first_part[0][::-1]
 		return second_half
 		
 	def _first_half_of_pyramid(self, start_counter, end_counter):
@@ -37,7 +37,7 @@ class Alphabet:
 			number_of_spaces = end_counter - letter_position - 1
 			space_in_between = 2 * multiplier + 1
 			current_letter = chr(letter_position + 1)
-			first_half += self._space(number_of_spaces) + current_letter + self._space(space_in_between) + current_letter + self._space(number_of_spaces) + '\n'
+			first_half += '\n' + self._space(number_of_spaces) + current_letter + self._space(space_in_between) + current_letter + self._space(number_of_spaces)
 			multiplier += 1
 		
 		return first_half
@@ -45,7 +45,7 @@ class Alphabet:
 	def _append_middle_pyramid(self, start_counter, end_counter):
 		first_half = self._first_half_of_pyramid(start_counter, end_counter)
 		second_half = self._reverse_pyramid(first_half)
-		return first_half + second_half 
+		return first_half + '\n' + second_half
 
 	def _formulate_output_string(self, input):
 		start_position = ord('A')
